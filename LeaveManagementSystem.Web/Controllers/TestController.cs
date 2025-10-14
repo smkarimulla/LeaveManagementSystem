68da6b34f7cad481737f1d303c00a6d0d2471117
smkarimulla <skmullakarimulla@gmail.com>
2025-10-14 12:18:25 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {        
+        public ActionResult Index()
+        {
+            var modelName = new Models.TestViewModel
+            {
+                Name = "Shaik Mulla Karimulla",
+                DateOfBirth = new DateOnly(2001,1,1)
+            };
+            return View(modelName);
+        }       
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
