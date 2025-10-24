daf00d2e15935e6e117b812ef693f1bc317c7e24
smkarimulla <skmullakarimulla@gmail.com>
2025-10-24 15:27:26 +0530

    Converted Leave Type Model Data into View Model Data

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
 
 namespace LeaveManagementSystem.Web.Controllers
 {
@@ -21,8 +22,17 @@ namespace LeaveManagementSystem.Web.Controllers
         // GET: LeaveTypes
         public async Task<IActionResult> Index()
         {
+            // var data = SELECT * FROM LeaveTypes
             var data = await _context.LeaveTypes.ToListAsync();
-            return View(data);
+            // conevert the data model into the view model
+            var ViewData = data.Select(q => new
+            IndexVM
+            {
+                Id = q.Id,
+                Name = q.Name,
+                Day = q.NumberOfDays
+            });
+            return View(ViewData);
         }
 
         // GET: LeaveTypes/Details/5
---
+++ LeaveManagementSystem.Web/Models/LeaveTypes/IndexVM.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagementSystem.Web.Models.LeaveTypes
+{
+    public class IndexVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Day { get; set; }
+
+    }
+}
